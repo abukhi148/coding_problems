addfb9f3d849cc844b2d4e9722bb102da8feace3
abukhari148 <m.ali.bukhari148@gmail.com>
2022-12-01 01:09:10 +0100

    testing aocd awesome tool

--- advent_of_code/2020/day1.py
+++ advent_of_code/2020/day1.py
@@ -1,4 +1,22 @@
 from aocd.models import Puzzle
-puzzle = Puzzle(year=2020, day=1)
-print(puzzle)
+from aocd import submit
+from itertools import combinations
+import math
 
+puzzle = Puzzle(year=2020, day=1)
+nums = list(map(int, puzzle.input_data.splitlines()))
+
+
+def part_a(nums):
+   for c in combinations(nums, 2):
+        if sum(c) == 2020:
+            return math.prod(c)
+
+
+def part_b(nums):
+    for c in combinations(nums, 3):
+        if sum(c) == 2020:
+            return math.prod(c)
+
+
+submit(part_b(nums), year=2020, day=1)
